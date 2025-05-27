0166086e0134cd0b151f8617f5eeaa93ccd0331f
Robert Rothenberg <rrwo@cpan.org>
2025-05-27 15:28:37 +0100

    Update copyright year

--- lib/Net/CIDR/Set.pm
+++ lib/Net/CIDR/Set.pm
@@ -794,7 +794,7 @@ Wilson's L<Net::CIDR::Lite>.
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
 
-Copyright (c) 2009, Message Systems, Inc.
+Copyright (c) 2009, 2014, 2025, Message Systems, Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or
